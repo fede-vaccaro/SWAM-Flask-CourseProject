7fe479b3418751738da490b6b2142761db814ee1
palace22 <palazzolo1995@gmail.com>
2020-07-08 22:15:00 +0200

    added toast controller

--- client/src/app/tabs/profile/profile.page.ts
+++ client/src/app/tabs/profile/profile.page.ts
@@ -4,13 +4,14 @@ import { User } from 'src/app/models/user';
 import { LoginService } from 'src/app/services/login.service';
 import { TicketService } from 'src/app/services/ticket.service';
 import { toggleDarkTheme } from 'src/util/utils';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-profile',
   templateUrl: 'profile.page.html',
   styleUrls: ['profile.page.scss']
 })
-export class ProfilePage{
+export class ProfilePage {
   user: User = {
     username: "",
     photoUrl: "",
@@ -18,16 +19,28 @@ export class ProfilePage{
 
   checked: boolean
 
-  constructor(private router: Router, private loginService: LoginService, private ticketService: TicketService) {
+  constructor(
+    private router: Router,
+    private loginService: LoginService,
+    private ticketService: TicketService,
+    public toastController: ToastController,
+  ) {
   }
-  
-  ionViewWillEnter(){
+
+  ionViewWillEnter() {
     this.checked = localStorage.getItem('enableDark') === 'true'
     this.user = this.loginService.getLoggedUser()
   }
 
   logout() {
     this.loginService.logout()
+  }
+
+  async remove() {
+    const isRemoved: boolean = await this.loginService.remove().toPromise()
+    if (isRemoved) {
+      this.presentToast('Account deleted!').then(() => this.logout())
+    }
   }
 
   goToAddFriends() {
@@ -49,5 +62,14 @@ export class ProfilePage{
   enableDark($event) {
     toggleDarkTheme($event.detail.checked)
     localStorage.setItem('enableDark', $event.detail.checked)
+  }
+
+  async presentToast(message: string, duration: number = 2000) {
+    const toast = await this.toastController.create({
+      message: message,
+      position: "middle",
+      duration: duration,
+    });
+    toast.present();
   }
 }
